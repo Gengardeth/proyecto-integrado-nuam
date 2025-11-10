6fab22dc3991bcea2a55744363b5f1ee030d9b27
KANON09 <claudioquilodran777@gmail.com>
2025-11-10 15:36:27 -0300

    postgre setiado

--- Nuam/settings.py
+++ Nuam/settings.py
@@ -77,13 +77,14 @@ WSGI_APPLICATION = 'Nuam.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'proyecto_nuam',
+        'USER': 'nuam_user',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
